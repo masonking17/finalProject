82c7411e5554e4e6f7905a8f08374580cedd8abb
Mason J. King <mason.king@centre.edu>
2019-11-21 10:56:29 -0500

    data in brackets

--- finalProject.js
+++ finalProject.js
@@ -1,1 +1,15 @@
+var promise = d3.csv("KenGriffeyJrData.csv");
 
+promise.then(function(data)
+
+{
+  console.log(data)
+})
+
+var promise = d3.csv("MikeTroutData.csv");
+
+promise.then(function(data)
+
+{
+    console.log(data)
+})
